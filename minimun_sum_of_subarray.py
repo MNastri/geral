a48a2165f73afd1080fdf59df78a2e6f70cb2986
MNastri <marcellonastri@gmail.com>
2022-03-19 16:15:31 -0300

    fix:solve edge case, remove clutter

--- minimun_sum_of_subarray.py
+++ minimun_sum_of_subarray.py
@@ -1,59 +1,56 @@
 """Learning Dynamic Programing."""
+from itertools import product
 from typing import (
     List,
     NamedTuple,
 )
 
-INPUT_ARRAYS = [[-7, 3, 4, -2, -3, 1, -3]]
+INPUT_ARRAYS = [
+    [-7, 3, 4, -2, -3, 1, -3],
+]
+INPUT_ARRAYS.extend([list(pp) for pp in product([-10, -1, 0, 1, 10], repeat=3)])
 
 
 class MinSubArray(NamedTuple):
-    index_start: int
-    index_end: int
-    sub_array_sum: int
+    start_index: int
+    end_index: int
+    sum: int
 
 
 def min_sum(array: List) -> MinSubArray:
     """
     Calculates the minimum sum of a continuous sub-array of array.
-    Returns a named tuple with attributes sub_array_sum, with the actual minimum
-    sum; index_start, with the starting index for the sub-array; and index_end,
+    Returns a named tuple with attributes sum, with the actual minimum
+    sum; start_index, with the starting index for the sub-array; and end_index,
     with the ending index for the sub-array.
     :param array:
     :return:
     """
-    print(array)
     if len(array) == 0:
-        return MinSubArray(0, 0, 0)
+        return MinSubArray(0, 1, 0)
     if len(array) == 1:
-        return MinSubArray(0, 0, array[0])
+        return MinSubArray(0, 1, array[0])
     mini_sum = float("inf")
     c_sum = float("inf")
     min_st_idx = 0
     min_en_idx = 0
     c_st_idx = 0
-    print("minimo global é", mini_sum)
-    print("minimo atual é", c_sum)
-
-    for current_end_index, number in enumerate(array):
-        print()
-        print("verifica", number)
-        c_sum += number
-        print("minimo atual é", c_sum)
+    for c_en_idx, number in enumerate(array):
         if number < c_sum:
-            print(number, "é menor que minimo atual")
             c_sum = number
-            c_st_idx = current_end_index
+            c_st_idx = c_en_idx
+        elif number >= c_sum:
+            c_sum += number
         if c_sum < mini_sum:
-            print("minimo atual é menor que minimo global")
             mini_sum = c_sum
             min_st_idx = c_st_idx
-            min_en_idx = current_end_index
-        print("minimo global é", mini_sum)
-    return MinSubArray(min_st_idx, min_en_idx, mini_sum)
+            min_en_idx = c_en_idx
+    return MinSubArray(min_st_idx, min_en_idx + 1, mini_sum)
 
 
 if __name__ == "__main__":
-    soma = min_sum(INPUT_ARRAYS)
     for array in INPUT_ARRAYS:
-        print(array, "=>", soma)
+        min_sum_array = min_sum(array)
+        st = min_sum_array.start_index
+        en = min_sum_array.end_index
+        print(array, "=>", array[st:en], "soma", min_sum_array.sum)
