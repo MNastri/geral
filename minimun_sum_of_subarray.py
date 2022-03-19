9ed607fdfd281c317e0afc13f66638caa163088b
MNastri <marcellonastri@gmail.com>
2022-03-19 14:36:49 -0300

    fix:use dynamic programing paradigm

--- minimun_sum_of_subarray.py
+++ minimun_sum_of_subarray.py
@@ -1,59 +1,59 @@
 """Learning Dynamic Programing."""
-# Standard Library
 from typing import (
-    Dict,
     List,
+    NamedTuple,
 )
 
-INPUT_ARRAY = [-7, 3, 4, -2, -3, 1, -3]
+INPUT_ARRAYS = [[-7, 3, 4, -2, -3, 1, -3]]
 
 
-def sum_slice(array: List[int], start: int, end: int, cache: Dict) -> int:
-    """
-    Sums a semi-open slice [start,end) of the given array of ints, checking the cache for hits.
-    cache is not working properly yet.
-    :param array:
-    :param start:
-    :param end:
-    :param cache:
-    :return:
-    """
-    sl = slice(start, end, None)
-    kk = str(sl)
-    if kk in cache:
-        # print(f"found sum for {sl} in cache, returning it. sum is {cache[kk]}")
-        return cache[kk]
-    cache[kk] = sum(array[sl])
-    # string_array = (str(array[sl]) + " " * 30)[:30]
-    # print(f"calculating sum for {sl} in array {string_array}...sum is {cache[kk]}")
-    return cache[kk]
+class MinSubArray(NamedTuple):
+    index_start: int
+    index_end: int
+    sub_array_sum: int
 
 
-# if __name__ == "__main__":
-#     cache = {str(slice(0, 1)): -7}
-#     print(sum_slice(INPUT_ARRAY, 0, 1, cache))
-#     print(sum_slice(INPUT_ARRAY, 0, 1, {}))
-#     del cache
-
-
-def min_sum(array: List) -> int:
+def min_sum(array: List) -> MinSubArray:
     """
     Calculates the minimum sum of a continuous sub-array of array.
-    returns the namedtuple with attributes value, with the actual minimum sum; start,
-    with the starting index for the sub-array; and end, with the ending index for the
-    sub-array.
+    Returns a named tuple with attributes sub_array_sum, with the actual minimum
+    sum; index_start, with the starting index for the sub-array; and index_end,
+    with the ending index for the sub-array.
     :param array:
     :return:
     """
-    cache = {}
-    result_sum = array[0]
-    ll = len(array)
-    for ss in range(ll):
-        for ee in range(ss, ll + 1):
-            result_sum = min(result_sum, sum_slice(array, ss, ee, cache))
-    return result_sum
+    print(array)
+    if len(array) == 0:
+        return MinSubArray(0, 0, 0)
+    if len(array) == 1:
+        return MinSubArray(0, 0, array[0])
+    mini_sum = float("inf")
+    c_sum = float("inf")
+    min_st_idx = 0
+    min_en_idx = 0
+    c_st_idx = 0
+    print("minimo global é", mini_sum)
+    print("minimo atual é", c_sum)
+
+    for current_end_index, number in enumerate(array):
+        print()
+        print("verifica", number)
+        c_sum += number
+        print("minimo atual é", c_sum)
+        if number < c_sum:
+            print(number, "é menor que minimo atual")
+            c_sum = number
+            c_st_idx = current_end_index
+        if c_sum < mini_sum:
+            print("minimo atual é menor que minimo global")
+            mini_sum = c_sum
+            min_st_idx = c_st_idx
+            min_en_idx = current_end_index
+        print("minimo global é", mini_sum)
+    return MinSubArray(min_st_idx, min_en_idx, mini_sum)
 
 
-# if __name__ == "__main__":
-#     result = min_sum(INPUT_ARRAY)
-#     print(result)
+if __name__ == "__main__":
+    soma = min_sum(INPUT_ARRAYS)
+    for array in INPUT_ARRAYS:
+        print(array, "=>", soma)
