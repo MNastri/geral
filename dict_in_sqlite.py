0ac59a280b6ad7596838508281adbd8d5aebd332
MNastri <marcellonastri@gmail.com>
2022-06-12 17:56:14 -0300

    chore:learning abc registering

--- dict_in_sqlite.py
+++ dict_in_sqlite.py
@@ -3,15 +3,20 @@ import sqlite3
 
 
 def learning_abc():
-    from abc import ABC, abstractmethod
+    from abc import (
+        ABC,
+        abstractmethod,
+    )
 
     class Entertainer(ABC):
         @abstractmethod
         def sing(self, song):
             pass
+
         @abstractmethod
         def dance(self):
             pass
+
         def all_the_other_stuff(self, msg):
             print(msg)
 
@@ -28,15 +33,41 @@ def learning_abc():
     print("Instance of StreetPerformer created. sp =", sp := StreetPerformer())
     print("Is sp an instance of StreetPerformer?", isinstance(sp, StreetPerformer))
     print("Is sp an instance of Entertainer?", isinstance(sp, StreetPerformer))
-    msg = """By subclassing Entertainer in StreetPerformer we added all the
+    msg = """\nBy subclassing Entertainer in StreetPerformer we added all the
 attributes from the Entertainer to the StreetPerformer. We implemented sing and 
 dance in StreetPerformer to override the ones in the base class, but we did not 
 implement all_the_other_stuff in StreetPerformer and we will get it for free.
 This means that subclassing can alter our class with the base class' attributes.
 We did not decorate all_the_other_stuff with abstractmethod or it would raise a
 TypeError at run time because all the decorated methods in the base class need 
-also to be created in their subclasses."""
+also to be defined in their subclasses.\n\n"""
     StreetPerformer().all_the_other_stuff(msg=msg)
+
+    class LoungePerformer:
+        def sing(self, song):
+            return f"Soft version of {song}"
+
+        def dance(self):
+            return f"Gentle moody dance"
+
+    Entertainer.register(LoungePerformer)
+    print(
+        "LoungePerformer is an Entertainer?", issubclass(StreetPerformer, Entertainer)
+    )
+    print("Instance of LoungePerformer created. lp =", lp := LoungePerformer())
+    print("Is lp an instance of LoungePerformer?", isinstance(lp, LoungePerformer))
+    print("Is lp an instance of Entertainer?", isinstance(lp, LoungePerformer))
+    print(
+        "is all_the_other_stuff in LoungePerformer?",
+        "all_the_other_stuff" in dir(LoungePerformer),
+    )
+    msg1 = """\nBy registering LoungPerformer as an Entertainer we can overload the 
+isinstance and issubclass functions to return True when checking if our class is
+an instance/subclass from the base class. This will be useful if we do not want 
+to add any attributes from the base class into our class. The abstractmethod
+decorator will not affect a registered subclass and will not raise any error.
+But the missing method will fail at runtime.\n\n"""
+    print(msg1)
 
 
 def main():
